16eca341d5fe0f3704ae8090b4677a14be460a88
aderyan18 <72294470+aderyan18@users.noreply.github.com>
2021-11-17 00:30:01 +0700

    update

--- src/View/obat/component/DaftarObat.js
+++ src/View/obat/component/DaftarObat.js
@@ -2,13 +2,24 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLOR } from "../../../styles/color";
-import { Divider, Button, Appbar, Avatar, Searchbar } from "react-native-paper";
-import { ScrollView } from "react-native-gesture-handler";
+import {
+  Divider,
+  Button,
+  Appbar,
+  Avatar,
+  Searchbar,
+  IconButton,
+} from "react-native-paper";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { Icon } from "react-native-vector-icons/FontAwesome";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 export default function DaftarObat() {
   const obat = [];
-  for (let a = 1; a < 4; a++) {
+  for (let a = 1; a < 2; a++) {
     obat[a] = a + 1;
   }
 
@@ -16,66 +27,107 @@ export default function DaftarObat() {
     <View
       style={{
         backgroundColor: COLOR.SECONDARY,
-        width: "100%",
-        height: "80%",
-        top: 15,
+        width: wp(100),
+        height: hp(100),
+        top: hp(3),
       }}
     >
       <View
         style={{
-          width: "90%",
-          height: 30,
+          width: wp(90),
+          height: hp(5),
           alignSelf: "center",
           alignItems: "center",
         }}
       >
-        <Text style={{ fontSize: 16, fontWeight: "bold" }}>Daftar Obat</Text>
+        <Text style={{ fontSize: wp(5), fontWeight: "bold" }}>Daftar Obat</Text>
       </View>
       <View
         style={{
-          width: "90%",
-          height: 350,
-          backgroundColor: "aqua",
+          width: wp(90),
+          height: hp(70),
+          backgroundColor: COLOR.SECONDARY,
           alignSelf: "center",
         }}
       >
         <ScrollView>
-          {obat.map(() => {
+          {obat.map((item, index) => {
             return (
               <View
                 style={{
-                  flexDirection: "row",
                   justifyContent: "space-between",
                 }}
+                key={index}
               >
                 <View
                   style={{
-                    width: 150,
-                    height: 160,
-                    backgroundColor: "red",
-                    marginBottom: 10,
+                    width: wp(100),
+                    height: hp(20),
+                    backgroundColor: "#ADB3BC",
+                    padding: wp(5),
+                    flexDirection: "row",
+                    // marginBottom: hp(10),
+                    // alignItems: "center",
                   }}
                 >
                   <Image
                     source={require("../../../assets/antalgin.jpg")}
                     style={{
-                      height: 120,
-                      width: 150,
+                      height: hp(15),
+                      width: wp(30),
                     }}
                   />
-                  <View>
-                    <Text>Detail</Text>
-                    {/* <Icon name="lock" size={10} color={"#686868"} /> */}
+                  <View
+                    style={{
+                      alignItems: "flex-start",
+                      justifyContent: "flex-start",
+                      width: wp(50),
+                      height: hp(15),
+                      backgroundColor: "red",
+                      padding: wp(3),
+                    }}
+                  >
+                    <Text style={{ fontSize: wp(5) }}>Antalgin</Text>
+                    <View
+                      style={{
+                        backgroundColor: "blue",
+                        width: wp(45),
+                        height: hp(8),
+                        alignSelf: "flex-end",
+                        flexDirection: "row",
+                        paddingTop: wp(3),
+                      }}
+                    >
+                      <TouchableOpacity
+                        style={{
+                          backgroundColor: "green",
+                          width: wp(10),
+                          height: hp(5),
+                          borderRadius: hp(10),
+                        }}
+                      ></TouchableOpacity>
+                      <View
+                        style={{
+                          backgroundColor: "white",
+                          width: wp(10),
+                          height: hp(5),
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <Text style={{ fontSize: hp(3) }}>1</Text>
+                      </View>
+                      <TouchableOpacity
+                        style={{
+                          backgroundColor: "aqua",
+                          width: wp(10),
+                          height: hp(5),
+                          borderRadius: hp(10),
+                        }}
+                      ></TouchableOpacity>
+                    </View>
                   </View>
                 </View>
-                <View
-                  style={{
-                    width: 150,
-                    height: 160,
-                    backgroundColor: "red",
-                    marginBottom: 10,
-                  }}
-                ></View>
               </View>
             );
           })}
