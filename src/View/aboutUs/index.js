8dc0adfdaa34999d48bd5dbe553b38d049f63482
aderyan18 <72294470+aderyan18@users.noreply.github.com>
2022-02-06 14:58:19 +0700

    update

--- src/View/aboutUs/index.js
+++ src/View/aboutUs/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLOR } from "../../styles/color";
 import { Divider, Button, Appbar, Avatar } from "react-native-paper";
@@ -65,7 +65,7 @@ export default function obat() {
           <View
             style={{
               width: wp(90),
-              height: hp(50),
+              height: hp(57),
               backgroundColor: "#FFF",
               marginTop: hp(3),
               borderRadius: wp(5),
@@ -82,6 +82,27 @@ export default function obat() {
             >
               Lokasi
             </Text>
+            <TouchableOpacity
+              // onPress={Linking.openURL(
+              //   "https://www.google.co.id/maps/place/Apotek+Assyifa+Medika/@-5.1358023,119.4943984,17z/data=!3m1!4b1!4m5!3m4!1s0x2dbefcb2523d1479:0xdffb25d5538e12fd!8m2!3d-5.1358872!4d119.4965157"
+              // )}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.google.co.id/maps/place/Apotek+Assyifa+Medika/@-5.1358023,119.4943984,17z/data=!3m1!4b1!4m5!3m4!1s0x2dbefcb2523d1479:0xdffb25d5538e12fd!8m2!3d-5.1358872!4d119.4965157"
+                )
+              }
+              style={{
+                backgroundColor: COLOR.SECONDARY,
+                width: wp(30),
+                height: hp(5),
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: wp(3),
+                marginLeft: wp(5),
+              }}
+            >
+              <Text>Lihat Lokasi</Text>
+            </TouchableOpacity>
             <Image
               source={require("../../assets/tes.png")}
               style={{
@@ -89,6 +110,7 @@ export default function obat() {
                 width: wp(80),
                 height: hp(40),
                 alignSelf: "center",
+                marginTop: hp(2),
               }}
             />
           </View>
